fe106579d8c4e1cf9485cbf13c72641f90d622a9
Titus Kovalik <titus.kovalik@tlgcohort.com>
2023-12-15 08:38:13 -0800

    variables now let instead of const to apply new

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ user_input_form.addEventListener("submit", (e) => {
     const destinationName = destination_name.value
     const locationName = location_name.value
     const photoUrl = photo_URL.value || PLACE_HOLDER_PHOTO_URL
-    const descript = description.value
+    let descript = description.value
 
     user_input_form.reset()
 
@@ -14,9 +14,39 @@ user_input_form.addEventListener("submit", (e) => {
 
     display_box.appendChild(card)
 
+    cards_container.addEventListener("click", (e) => {
+        const clickedElt = e.target
+
+        if (clickedElt.getAttribute("btn_type") === "delete") {
+            clickedElt.parentElement.parentElement.remove()
+        } else if (clickedElt.getAttribute("btn_type") === "edit") {
+            handleEdit(clickedElt)
+        }
+    })
+
+    function handleEdit(editBtn) {
+        const cardBody = editBtn.parentElement
+        const oldDestName = cardBody.children[0].textContent
+        const oldLocName = cardBody.children[1].textContent
+        const oldPhotoUrl = cardBody.previousSiblingElement.getAttribute("src")
+
+        const oldDesc = cardBody.children[2].tagName === "p" ? cardBody.children[2].textContent : ""
+        
+        const newDestName = prompt("Enter new destination name", oldDestName)
+        const newLocName = prompt("Enter new location name", oldLocName)
+        const newPhotoUrl = prompt("Enter new photo URL", oldPhotoUrl)
+        const newDesc = prompt("Enter new description", oldDesc)
+        if (newDestName && newDestName !== oldDestName){
+            cardBody.children[0].textContent = newDestName
+        }
+    }
+
     delete_button.addEventListener("click", function () {
         card.remove()
     })
+    
+  
+    
 })
 
 function createCard({destinationName, locationName, photoUrl, descript}) {   
@@ -31,7 +61,7 @@ function createCard({destinationName, locationName, photoUrl, descript}) {
     <h5 class="card-title">${destinationName}</h5>
     <p class="card-text">${locationName}</p>
     ${descript && `<p class="card-text"}>${descript}</p>`}
-    <button type="button" class="btn btn-info">Edit</button>
+    <button type="button" class="btn btn-info" id="edit_button">Edit</button>
     <button type="button" class="btn btn-danger" id="delete_button" >Delete</button>
     </div>`
     return card
